fadc7821c7b9c405e7c07827b41e953a4b1303a7
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2020-08-13 19:52:51 -0600

    Update prod config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,26 +1,16 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
 use Mix.Config
-
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
 
 config :newton, Newton.Repo,
   # ssl: true,
-  url: database_url,
+  username: "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || raise("No db password set in env"),
+  database: "newton_prod",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
     raise """
     environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
     """
 
 config :newton, NewtonWeb.Endpoint,
