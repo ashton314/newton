7eb5eab23835b99ba0b2040de3f3aff306895849
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2020-08-28 00:31:49 -0600

    Now runs in container with release

--- config/prod.exs
+++ config/prod.exs
@@ -10,11 +10,10 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :newton, NewtonWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-config :newton, Newton.Repo,
-  hostname: "db"
+config :newton, Newton.Repo, hostname: "db"
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/releases.exs
+++ config/releases.exs
@@ -25,7 +25,7 @@ config :newton, NewtonWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :newton, NewtonWeb.Endpoint, server: true
+config :newton, NewtonWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
