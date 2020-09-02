2e7a2d198bfa0b24b799fea0b43ce5164e6ae680
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2020-09-01 22:43:47 -0600

    update args for convert

--- lib/latex_renderer.ex
+++ lib/latex_renderer.ex
@@ -186,6 +186,12 @@ defmodule LatexRenderer do
              # "-resize",
              # "1500x1500",
 
+             # Prevent OOM-killing
+             "-limit",
+             "memory",
+             "40MB",
+
+             # Begin prod settings
              "-density",
              "3000x3000",
              "#{dir}/#{base_file}.pdf",
