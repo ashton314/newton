e4336aa3216a19e2bd70c74a977d0dfc44cd15f9
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2020-12-31 14:00:33 -0700

    Remove tests that I don't care about any more

--- test/newton_web/live/exam_live_test.exs
+++ test/newton_web/live/exam_live_test.exs
@@ -13,15 +13,16 @@ defmodule NewtonWeb.ExamLiveTest do
     name: "some name",
     stamp: "some stamp"
   }
-  @update_attrs %{
-    barcode: "some updated barcode",
-    course_code: "some updated course_code",
-    course_name: "some updated course_name",
-    exam_date: "some updated exam_date",
-    name: "some updated name",
-    stamp: "some updated stamp"
-  }
-  @invalid_attrs %{barcode: nil, course_code: nil, course_name: nil, exam_date: nil, name: nil, stamp: nil}
+
+  # @update_attrs %{
+  #   barcode: "some updated barcode",
+  #   course_code: "some updated course_code",
+  #   course_name: "some updated course_name",
+  #   exam_date: "some updated exam_date",
+  #   name: "some updated name",
+  #   stamp: "some updated stamp"
+  # }
+  # @invalid_attrs %{barcode: nil, course_code: nil, course_name: nil, exam_date: nil, name: nil, stamp: nil}
 
   defp fixture(:exam) do
     {:ok, exam} = Exam.create_exam(@create_attrs)
@@ -36,12 +37,12 @@ defmodule NewtonWeb.ExamLiveTest do
   describe "Index" do
     setup [:create_exam]
 
-    test "lists all exams", %{conn: conn, exam: exam} do
-      {:ok, _index_live, html} = live(conn, Routes.exam_index_path(conn, :index))
+    # test "lists all exams", %{conn: conn, exam: exam} do
+    #   {:ok, _index_live, html} = live(conn, Routes.exam_index_path(conn, :index))
 
-      assert html =~ "Listing Exams"
-      assert html =~ exam.barcode
-    end
+    #   assert html =~ "Listing Exams"
+    #   assert html =~ exam.barcode
+    # end
 
     # @tag :skip
     # test "saves new exam", %{conn: conn} do
@@ -66,27 +67,27 @@ defmodule NewtonWeb.ExamLiveTest do
     #   assert html =~ "some barcode"
     # end
 
-    test "updates exam in listing", %{conn: conn, exam: exam} do
-      {:ok, index_live, _html} = live(conn, Routes.exam_index_path(conn, :index))
+    # test "updates exam in listing", %{conn: conn, exam: exam} do
+    #   {:ok, index_live, _html} = live(conn, Routes.exam_index_path(conn, :index))
 
-      assert index_live |> element("#exam-#{exam.id} a", "Edit") |> render_click() =~
-               "Edit Exam"
+    #   assert index_live |> element("#exam-#{exam.id} a", "Edit") |> render_click() =~
+    #            "Edit Exam"
 
-      assert_patch(index_live, Routes.exam_index_path(conn, :edit, exam))
+    #   assert_patch(index_live, Routes.exam_index_path(conn, :edit, exam))
 
-      assert index_live
-             |> form("#exam-form", exam: @invalid_attrs)
-             |> render_change() =~ "can&apos;t be blank"
+    #   assert index_live
+    #          |> form("#exam-form", exam: @invalid_attrs)
+    #          |> render_change() =~ "can&apos;t be blank"
 
-      {:ok, _, html} =
-        index_live
-        |> form("#exam-form", exam: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.exam_index_path(conn, :index))
+    #   {:ok, _, html} =
+    #     index_live
+    #     |> form("#exam-form", exam: @update_attrs)
+    #     |> render_submit()
+    #     |> follow_redirect(conn, Routes.exam_index_path(conn, :index))
 
-      assert html =~ "Exam updated successfully"
-      assert html =~ "some updated barcode"
-    end
+    #   assert html =~ "Exam updated successfully"
+    #   assert html =~ "some updated barcode"
+    # end
 
     test "deletes exam in listing", %{conn: conn, exam: exam} do
       {:ok, index_live, _html} = live(conn, Routes.exam_index_path(conn, :index))
